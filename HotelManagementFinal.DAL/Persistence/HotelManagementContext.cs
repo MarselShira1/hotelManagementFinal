008944a823c6de88ac69ce315ccbfd3d6a34bcbc
XheniMalo <xhensilamalo1@gmail.com>
2025-01-19 18:43:30 +0100

    room rate ranges

--- HotelManagementFinal.DAL/Persistence/HotelManagementContext.cs
+++ HotelManagementFinal.DAL/Persistence/HotelManagementContext.cs
@@ -48,7 +48,7 @@ public partial class HotelManagementContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-F8EDG2P;Database=HotelManagement;User ID=sa;Password=Ester123;Trusted_Connection=False;TrustServerCertificate=Yes");
+        => optionsBuilder.UseSqlServer("Server=XHENI;Database=HotelManagement;User ID=sa;Password=xhenixheni2003;Trusted_Connection=False;TrustServerCertificate=Yes");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
