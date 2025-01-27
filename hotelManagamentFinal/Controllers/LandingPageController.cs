961218117e520af94cb1432536a34997286bd7b1
XheniMalo <xhensilamalo1@gmail.com>
2025-01-27 22:58:28 +0100

    fronti

---
+++ hotelManagamentFinal/Controllers/LandingPageController.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementISE.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        private readonly ILogger<LandingPageController> _logger;
+
+        public LandingPageController(ILogger<LandingPageController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
