86c050d3ad173bc5db838980a2c3c852178b42f0
XheniMalo <xhensilamalo1@gmail.com>
2025-01-30 23:32:07 +0100

    frontii

--- hotelManagamentFinal/Controllers/LandingPageController.cs
+++ hotelManagamentFinal/Controllers/LandingPageController.cs
@@ -1,15 +1,20 @@
 using System.Diagnostics;
+using hotelManagement.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
+using hotelManagement.DAL.Persistence.Entities;
 
 namespace HotelManagementISE.Controllers
 {
     public class LandingPageController : Controller
     {
         private readonly ILogger<LandingPageController> _logger;
+        private readonly IRoomTypeService _roomType;
 
-        public LandingPageController(ILogger<LandingPageController> logger)
+        public LandingPageController(ILogger<LandingPageController> logger, IRoomTypeService roomType)
         {
             _logger = logger;
+            _roomType = roomType;
         }
 
         public IActionResult Index()
@@ -19,12 +24,24 @@ namespace HotelManagementISE.Controllers
 
         public IActionResult Rooms()
         {
-            return View();
+            var rooms = _roomType.GetAllRoomTypes();
+            return View(rooms);
         }
         public IActionResult Reservations()
         {
             return View();
         }
 
+        public IActionResult AddAccomodation(Akomodim akomodimi)
+        {
+
+            return View("Index");
+        }
+
+        public JsonResult CheckAvailability(DateTime checkinDate, DateTime checkoutDate, int adults, int children)
+        {
+            var rooms = _roomType.GetAllRoomTypes();
+            return Json(rooms);
+        }
     }
 }
